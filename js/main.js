64544bfa0d9599efb0ce2d2e4b0d52cf64c472fc
Jota Nascimento <jrnascimento09@gmail.com>
2020-10-18 14:07:43 -0300

    Finalizando Aula

--- js/main.js
+++ js/main.js
@@ -1,3 +1,56 @@
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por Clicar");
+}
+
+function redirecionar() {
+    window.open("https://neoxscans.com/manga/56789/"); //abrir em uma nova janela
+    //window.location.href = "https://neoxscans.com/manga/56789/"; //abrir na mesma janela
+}
+
+function trocar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+    elemento.innerHTML = "Obrigado por passar o mouse"
+    //alert("trocar texto");
+}
+
+function voltar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+    elemento.innerHTML = "Passe o mouse aqui"
+}
+
+function load() {
+    alert("Pagina carregada");
+}
+
+function funcaoChange(elemento) {
+    console.log(elemento.value);
+}
+
+
+/*
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+
+var validar= 0;
+
+function validaIdade(idade) {
+    if (idade >= 18) {
+        validar = true
+    }
+    else { 
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+validaIdade(idade);
+console.log(validar);
+*/
 
 /*
 var d = new Date();

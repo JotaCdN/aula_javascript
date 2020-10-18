14ebf923802a80a42fdf733619963946e92b7de8
Jota Nascimento <jrnascimento09@gmail.com>
2020-10-18 04:47:59 -0300

    aula

---
+++ js/main.js
@@ -0,0 +1,68 @@
+
+/*
+var d = new Date();
+alert(d.getTime());
+*/
+
+/*
+var count;
+for(count=1; count <= 5; count++) {
+    alert(count);
+}
+*/
+
+
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18) {
+    alert("maior de idade");
+}
+else {
+    alert("menor de idade");
+}
+*/
+
+
+/*
+var frutas = [{nome:"maca", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+
+/*
+var fruta = {nome:"maca", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+//var lista = ["maca", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+//var nome = "Jota Nascimento";
+//var idade = 23;
+//var idade2 = 10;
+//var frase = "Japao e o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos")
+//alert(idade + idade2)
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japao", "Brasil"));
